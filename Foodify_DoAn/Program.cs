3d9eecfc38ae36d776a619dd7d92858f29774209
Minh Khoa <tukhoa040505@gmail.com>
2025-04-15 09:18:27 +0700

    update program.cs

--- Foodify_DoAn/Program.cs
+++ Foodify_DoAn/Program.cs
@@ -67,9 +67,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
-app.MapGet("/", () => Results.Redirect("/swagger"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
